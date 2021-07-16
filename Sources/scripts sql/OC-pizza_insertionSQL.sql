46eef385346122573fb3240f0af5199523ae7491
Paul Ghibs <paul.ghib@gmail.com>
2021-07-16 19:49:34 +0200

    Update OC-pizza_insertionSQL.sql

--- Sources/scripts sql/OC-pizza_insertionSQL.sql
+++ Sources/scripts sql/OC-pizza_insertionSQL.sql
@@ -27,69 +27,48 @@ VALUES
 -- Table `ocpizza`.`restaurant`
 -- -----------------------------------------------------
 INSERT INTO `oc-pizza`.Restaurant
-	(`refRestaurant`, `refAdresse`, `refNotif`, `faq`, `nombre employés`, `idCommande`)
+	(`refRestaurant`, `refAdresse`, `refNotif`,`nom`, `nombre employés`, `adresse`,`faq`, `Commande_IdCommande`, `Employé_IdEmployé`, `Adresse_refAdresse`)
 VALUES
-	(1, '0173456989', '#58979', 'Lorem Ipsum questionate', '2', '1790' ),
-	(2, '0323456189', '#28098', 'Lorem Ipsum questionate', '6','7090'),
-	(3, '0623656739', '#78639', 'Lorem Ipsum questionate', '2', NULL)
+	(1, '0173456989', '#58979', 'OCpizza_parmentier', '2', 'Parmentier', 'Lorem Ipsum questionate', '70990', '277','1'),
+	(2, '0323456189', '#28098', 'OCpizza_SaintDenis', '3', 'SaintDenis', 'Lorem Ipsum questionate', '68003', '276', '2'),
+	(3, '0623656739', '#78639', 'OCpizza_VictorHugo', '6', 'VictorHugo', 'Lorem Ipsum questionate', '56009', '275', '3')
 	;
 
 -- -----------------------------------------------------
 -- Table `ocpizza`.`commande`
 -- -----------------------------------------------------
 	INSERT INTO `oc-pizza`.Commande
-		(`idCommande`, `dateCommande`, `valeurPanier`, `reduction`, `livraison`)
+		(`idCommande`, `dateCommande`, `valeurPanier`, `reduction`, `livraison`, `Adresse_refAdresse`)
 	VALUES
-		('1790', '2018-07-14 12:46:28', '25,00', NULL, 'TRUE'),
-		('7090', '2018-07-13 13:02:12', '11,00', NULL, 'FALSE')
+		('1790', '2018-07-14 12:46:28', '25', NULL, '1', '1'),
+		('7090', '2018-07-13 13:02:12', '11', NULL, '0', '2')
 	;
 
 -- -----------------------------------------------------
 --  Table `Statut Commande`
 -- -----------------------------------------------------
-INSERT INTO `ocpizza`.`article`
-	(`idStatut`,`etat_paiement`, `etat_preparation`, `etat_livraison`)
+INSERT INTO `oc-pizza`.`Statut Commande`
+	(`idStatut Commande`,`etatCreation`,`etatPaiement`, `etatPreparation`, `etatLivraison`)
 VALUES
-	(1, '1', '12,00', 1),
-	(2, '1', '6,00', 1),
-	(3, '2', '7,00', 1),
-	(4, '1', '11,00', 2)
+	(1, '2021-07-14 12:43:28', 1 , '2018-07-14 12:54:28','2018-07-14 13:03:28'),
+	(2, '2021-07-14 13:26:28', 1, '2018-07-14 13:40:28','2018-07-14 14:00:28'),
+	(3, '2021-07-14 14:12:28', 1, '2018-07-14 14:16:28', '2018-07-14 14:36:28'),
+	(4, '2021-07-14 15:46:28',  2, '2018-07-14 15:50:28', '2018-07-14 16:00:28')
 ;
 
 
--- -----------------------------------------------------
--- Table `ocpizza`.`ingredient`
--- -----------------------------------------------------
-INSERT INTO `ocpizza`.`ingredient`
-	(`idIngrédients`, `maj`, `refonteStock`)
-VALUES
-	(1, 'anchois', '22/07'),
-	(2, 'câpres', '23/09'),
-	(3, 'mozzarella', '22/06'),
-	(4, 'roquette', '24/12'),
-	(5, 'champignon', '23/04')
-;
 
 -- -----------------------------------------------------
 -- Table `ocpizza`.`employe`
 -- -----------------------------------------------------
-INSERT INTO `ocpizza`.`employe`
-	(`idemploye`, `refCommande`, `fonction`)
+INSERT INTO `oc-pizza`.Employé
+	(`idEmployé`, `refCommande`, `fonction`)
 VALUES
 	(1, 'Richard', 'Livreur'),
 	(2, 'Erlich', 'Préparateur'),
 	(3, 'Bertram', 'responsable')
 ;
 
--- -----------------------------------------------------
--- Table `ocpizza`.`recette`
--- -----------------------------------------------------
-INSERT INTO `ocpizza`.`recette`
-	(`idrecettes`, `nom`, `tempsEstimé`)
-VALUES
-	(1, 'pizza napolitaine', '420'),
-	(2, 'pizza roma', '450')
-;
 
 -- -----------------------------------------------------
 -- Table `ocpizza`.`notification`
@@ -111,15 +90,6 @@ VALUES
 	('72379', '"39870')
 ;
 
--- -----------------------------------------------------
--- Table `Client_has_Commande`
--- -----------------------------------------------------
-INSERT INTO `ocpizza`.`restaurant_has_client`
-	(`Client_idClient`, `Commande_idCommande`)
-VALUES
-	(1, 2),
-	(2, 3)
-;
 
 
 
